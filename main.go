4ecb4407846cb714de42f411b7050686571d49d0
muscle <814588133@qq.com>
2024-09-27 11:00:49 +0800

    111

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 
 	ginServer := gin.Default()
 	ginServer.GET("/hello", func(context *gin.Context) {
-		context.JSON(200, gin.H{"msg": "hello go  wdaddaw"})
+		context.JSON(200, gin.H{"msg": "这是git上传的测试"})
 	})
 
 	ginServer.Run(":8888")
